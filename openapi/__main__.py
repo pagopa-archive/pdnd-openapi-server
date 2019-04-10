034b54da93e8f4b95ccc595e8ef63a93a712b71a
Roberto Polli <roberto.polli@par-tec.it>
2019-04-10 23:14:10 +0200

    Fix: #11. Reorganized api inside openapi module.

--- openapi/__main__.py
+++ openapi/__main__.py
@@ -66,6 +66,6 @@ def dataset_save_test(file):
 
 
 if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__, specification_dir='openapi/', options={"swagger_ui": True})
+    app = connexion.FlaskApp(__name__, specification_dir='.', options={"swagger_ui": True})
     app.add_api('daf-openapi.yaml') #, validate_responses=True)
     app.run(port=8080)
