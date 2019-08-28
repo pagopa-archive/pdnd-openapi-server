29a965b68e96c2e18e81cf713e3f3f36c2f80c25
Alessandro Ercolani <ale.ercolani@gmail.com>
2019-08-28 17:14:06 +0200

    test nteract

--- openapi/__main__.py
+++ openapi/__main__.py
@@ -1,5 +1,5 @@
 import logging
-
+from flask_cors import CORS
 import connexion
 
 logging.basicConfig(level=logging.DEBUG)
@@ -9,5 +9,6 @@ if __name__ == "__main__":
     app = connexion.FlaskApp(
         __name__, specification_dir=".", options={"swagger_ui": True}
     )
+    CORS(app.app)
     app.add_api("daf-openapi.yaml")  # , validate_responses=True)
     app.run(port=8080)
