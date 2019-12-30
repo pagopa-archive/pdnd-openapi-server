57798b253692ce2df084330f9fa1a9fc8c199809
Alessandro Ercolani <ale.ercolani@gmail.com>
2019-12-30 10:31:19 +0100

    integration with ckan

--- ckan/ckan_integration.py
+++ ckan/ckan_integration.py
@@ -3,7 +3,8 @@ import requests
 
 def public_search(filters, headers):
     name = filters['text']
-    url = 'https://ckan-geo.daf.teamdigitale.it/api/3/action/resource_search?query=name:' + name 
+    #url = 'https://ckan-geo.daf.teamdigitale.it/api/3/action/resource_search?query=name:' + name 
+    url = 'https://ckan.dev.pdnd.italia.it/api/3/action/resource_search?query=name:' + name
     print(url)
     r = requests.get(url)
     if r.status_code == 200:
