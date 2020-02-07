06be1d25b4f7737593cda37bc3e82c72fe65e566
Alessandro Ercolani <ale.ercolani@gmail.com>
2020-02-07 10:43:29 +0100

    connect to data.gov

--- ckan/ckan_integration.py
+++ ckan/ckan_integration.py
@@ -4,13 +4,20 @@ import requests
 def public_search(filters, headers):
     name = filters['text']
     #url = 'https://ckan-geo.daf.teamdigitale.it/api/3/action/resource_search?query=name:' + name 
-    url = 'https://ckan.dev.pdnd.italia.it/api/3/action/resource_search?query=name:' + name
+    #url = 'https://ckan.dev.pdnd.italia.it/api/3/action/resource_search?query=name:' + name
+    url = 'https://catalog.data.gov/api/3/action/package_search?q=' + name
     print(url)
     r = requests.get(url)
     csvs = []
     if r.status_code == 200:
         result = r.json()['result']
         if result['count'] > 0:
+            print(result['count'])
             results = result['results']
-            csvs = [{'name': x['name'], 'url': x['url']} for x in results if x['format'].lower() == 'csv']
+            # only for  data.gov
+            for res in results:
+                resources = res['resources']
+                csv = [{'name': x['name'], 'url': x['url']} for x in resources if x['format'].lower() == 'csv']
+                for info in csv:
+                    csvs.append(info)
     return csvs
